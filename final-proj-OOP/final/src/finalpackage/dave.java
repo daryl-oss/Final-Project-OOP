d8e20213c2cea2d66e24536e284806a3e6f95400
darlyyyyy <darylcarillo2006@gmail.com>
2025-06-01 18:04:55 -1100

    Add files via upload

---
+++ final-proj-OOP/final/src/finalpackage/dave.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package finalpackage;
+
+/**
+ *
+ * @author USER
+ */
+public class dave {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+ main open = new main();
+        open.setVisible(true);   
+    }
+    
+}
